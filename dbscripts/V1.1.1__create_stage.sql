71234e734fc706a00ab0bb0232f6056014d59a5d
HariomJa2 <hariomjangid687@gmail.com>
2023-01-09 18:54:04 +0530

    Create V1.1.1__create_stage.sql

---
+++ dbscripts/V1.1.1__create_stage.sql
@@ -0,0 +1,13 @@
+create database if not exists CICD_TEST_DB;
+
+create schema if not exists CICD_TEST_DB.CICD_SCHEMA;
+
+create table if not exists CICD_TEST_DB.CICD_SCHEMA.EMP_TEST(
+  firstname string,
+  lastname string
+);
+
+insert overwrite into CICD_TEST_DB.CICD_SCHEMA.EMP_TEST values('Hariom', 'jangir');
+
+create stage if not exists CICD_TEST_DB.CICD_SCHEMA.TEST_STAGE;
+
