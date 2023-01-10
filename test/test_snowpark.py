dc7a3c09249b076266ff6702519b0098b5900a4c
HariomJa2 <hariomjangid687@gmail.com>
2023-01-10 12:58:37 +0530

    Update test_snowpark.py

--- test/test_snowpark.py
+++ test/test_snowpark.py
@@ -1,15 +1,14 @@
 def run(session):
-    df_snowpark_curated = session.table("snowpark_curated")
-    df_snowpark_delta = session.table("snowpark_delta")
+    df_snowpark_curated = session.table("customer")
+    df_snowpark_delta = session.table("orders")
     try:
         df_snowpark_final = df_snowpark_curated.join(df_snowpark_delta,
-        (df_snowpark_delta.col("customer_id")==df_snowpark_curated.col("customer_id"))
-& (df_snowpark_delta.col("transaction_id")==df_snowpark_curated.col("transaction_id"))).select (df_snowpark_curated.col("customer_id"),df_snowpark_curated.col("transaction_id"),
-        (df_snowpark_delta.col("amount")+df_snowpark_curated.col("amount")).alias ("amount")) 
+        (df_snowpark_delta.col("c_custkey")==df_snowpark_curated.col("c_custkey"))).select (df_snowpark_curated.col("c_custkey"),df_snowpark_curated.col("c_custkey"),
+        (df_snowpark_delta.col("o_totalprice")-df_snowpark_curated.col("c_acctbal")).alias ("avl_bal")) 
     except:
         return "curation unsuccessfull"
     else:
         pass
 
-    df_snowpark_final.write.mode("overwrite").save_as_table("snowpark_curated_1")
+    df_snowpark_final.write.mode("overwrite").save_as_table("snowpark_curated_2")
     return "curation successfull"
