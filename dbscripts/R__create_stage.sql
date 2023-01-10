054ad7255b9ea795120540c2d56bc6131c05f623
HariomJa2 <hariomjangid687@gmail.com>
2023-01-10 11:56:32 +0530

    Update R__create_stage.sql

--- dbscripts/R__create_stage.sql
+++ dbscripts/R__create_stage.sql
@@ -9,11 +9,11 @@ create table if not exists CICD_TEST_DB.CICD_SCHEMA.EMP_TEST(
 
 insert overwrite into CICD_TEST_DB.CICD_SCHEMA.EMP_TEST values('Hariom', 'jangir'),('H', 'Jangid');
 
-create or replace file format none_compression
+create or replace file format CICD_TEST_DB.CICD_SCHEMA.NONE_COMPRESSION
 compression = None;
 
-create stage if not exists CICD_TEST_DB.CICD_SCHEMA.TEST_STAGE
-file_format = none_compression;
+create or replace stage CICD_TEST_DB.CICD_SCHEMA.TEST_STAGE
+file_format = CICD_TEST_DB.CICD_SCHEMA.NONE_COMPRESSION
 
 put file://test/* @CICD_TEST_DB.CICD_SCHEMA.TEST_STAGE;
 
