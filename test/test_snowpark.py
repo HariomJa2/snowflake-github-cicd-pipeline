829e02b1c50cf0abfb9b9d88ec3e3b5368f441e7
supsmaid <122077806+supsmaid@users.noreply.github.com>
2023-01-10 19:09:10 +0530

    Update test_snowpark.py

--- test/test_snowpark.py
+++ test/test_snowpark.py
@@ -2,9 +2,7 @@ def run(session):
     df_snowpark_curated = session.table("customer")
     df_snowpark_delta = session.table("orders")
     try:
-        df_snowpark_final = df_snowpark_curated.join(df_snowpark_delta,
-        (df_snowpark_delta.col("o_custkey")==df_snowpark_curated.col("c_custkey"))).select(df_snowpark_curated.col("c_custkey"),
-        df_snowpark_delta.col("o_totalprice")) 
+        df_snowpark_final =select(df_snowpark_curated.col("c_custkey")) 
     except:
         return "curation unsuccessfull"
     else:
