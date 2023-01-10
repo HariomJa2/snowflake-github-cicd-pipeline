7fa5e7ac2301b3744e3a6e529cc7ec382f35fb9e
HariomJa2 <hariomjangid687@gmail.com>
2023-01-10 13:13:14 +0530

    Added code for orders and customer table

--- test/test_snowpark.py
+++ test/test_snowpark.py
@@ -3,7 +3,7 @@ def run(session):
     df_snowpark_delta = session.table("orders")
     try:
         df_snowpark_final = df_snowpark_curated.join(df_snowpark_delta,
-        (df_snowpark_delta.col("c_custkey")==df_snowpark_curated.col("c_custkey"))).select (df_snowpark_curated.col("c_custkey"),df_snowpark_curated.col("c_custkey"),
+        (df_snowpark_delta.col("c_custkey")==df_snowpark_curated.col("c_custkey"))).select (df_snowpark_curated.col("c_custkey"),
         (df_snowpark_delta.col("o_totalprice")-df_snowpark_curated.col("c_acctbal")).alias ("avl_bal")) 
     except:
         return "curation unsuccessfull"
