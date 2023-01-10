8abf4a2f3ff5caae6d7ce2d14d0d17fd1ea5f32f
supsmaid <122077806+supsmaid@users.noreply.github.com>
2023-01-10 19:17:13 +0530

    Update R__create_stage.sql

--- dbscripts/R__create_stage.sql
+++ dbscripts/R__create_stage.sql
@@ -15,5 +15,5 @@
 -- create or replace stage CICD_TEST_DB.CICD_SCHEMA.TEST_STAGE
 -- file_format = CICD_TEST_DB.CICD_SCHEMA.NONE_COMPRESSION;
 
-put file://test/test_snowpark.py @DEMO_DB.PUBLIC.snowpark_internal_stage AUTO_COMPRESS = FALSE SOURCE_COMPRESSION = NONE OVERWRITE = TRUE;
+put file://test/test_snowpark_2.py @DEMO_DB.PUBLIC.snowpark_internal_stage AUTO_COMPRESS = FALSE SOURCE_COMPRESSION = NONE OVERWRITE = TRUE;
 
